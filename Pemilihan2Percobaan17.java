ee2d77546d6f1f9199ecafd870c6affd2097cff6
SaSaaaaaaaaa <shrxnz2332@gmail.com>
2024-10-06 17:18:25 +0700

    Percobaan 1, modifikasi nomor 3

--- Pemilihan2Percobaan17.java
+++ Pemilihan2Percobaan17.java
@@ -1,18 +1,18 @@
-import java.util.Scanner;
-class Pemilihan2Percobaan17{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int tahun;
+import java.util.Scanner; // import Scanner
+public class Pemilihan2Percobaan17{ // nama class bersifat public
+    public static void main(String[] args) { 
+        Scanner scan = new Scanner(System.in); // deklarasi Scanner dengan nama scan 
+        int tahun; // variabel tahun bertipe data integer
+ 
+        System.out.print("Masukkan tahun : "); // input tahun
+        tahun = scan.nextInt(); // fungsi input tahun
 
-        System.out.print("Masukkan tahun : ");
-        tahun = scan.nextInt();
-
-        if((tahun % 4) == 0){
-            if((tahun % 100) == 0){
-                System.out.println("Bukan tahun kabisat");
+        if((tahun % 4) == 0){ // apabila tahun dibagi empat dan hasil sisa baginya sama dengan 0, maka
+            if((tahun % 100) == 0){ // dan apabila tahun hasil modulusnya dengan 100 sama dengan 0, maka
+                System.out.println("Bukan tahun kabisat"); // outputnya berupa bukan tahun kabisat
             }
         }else{
-            System.out.println("Bukan tahun kabisat");
+            System.out.println("Bukan tahun kabisat"); // selain itu, maka outputnya bukan tahun kabisat
         }
     }
 }
