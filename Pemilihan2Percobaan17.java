c95a6a0ad18f35371512cb0550a9a2428cd1f7f6
SaSaaaaaaaaa <shrxnz2332@gmail.com>
2024-10-06 16:28:57 +0700

    Percobaan 1 nomor 3

---
+++ Pemilihan2Percobaan17.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Pemilihan2Percobaan17{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan tahun : ");
+        tahun = scan.nextInt();
+
+        if((tahun % 4) == 0){
+            if((tahun % 100) != 0){
+                System.out.println("Ini tahun kabisat");
+            }
+            else{
+                System.out.println("Bukan tahun kabisat");
+            }
+        }else{
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
